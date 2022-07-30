ade5c628d2a34e0759192e5f1ac9d822c023db49
Zach Chen <zachxdc@gmail.com>
2022-07-30 21:26:58 +1000

    Calculate the product quantity

--- src/checkout.js
+++ src/checkout.js
@@ -1,18 +1,39 @@
 export class Checkout {
-  constructor(pricingRules)
-  {
+  constructor(pricingRules) {
     this.adCart = [];
-    this.adQuantity = [];
     this.pricingRules = pricingRules;
   }
 
   add(ad) {
-    this.adCart.push(ad);
-    this.adQuantity[ad.name] += 1;
+    const index = this.adCart.findIndex((item) => {
+      return item.name === ad.name;
+    });
+    if (index === -1) {
+      // not in
+      this.adCart.push({
+        ...ad,
+        quantity: 1,
+      });
+    } else {
+      // in
+      this.adCart[index].quantity++;
+    }
   }
 
   total() {
-    const totalPrice = this.adCart.reduce((total, ad) => total + (ad.price * this.pricingRules)/100, 0);
-    return totalPrice
+    const totalPrice = this.adCart.reduce(
+      (total, ad) => total + (ad.price * ad.quantity * this.pricingRules) / 100,
+      0
+    );
+    return totalPrice;
   }
+
+  // total() {
+  //   const totalPrice = 0;
+  //   adCart.forEach(ad => {
+  //     rules.forEach(rule => {
+  //     })
+  //   })
+  //   return totalPrice;
+  // }
 }
